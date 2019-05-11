225502f3edffe20a21690f29ed64d093e77cab85
stoic-plus <ricardoledesmadev@gmail.com>
2019-05-11 15:04:11 -0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('App.js is running');
+
+var template = <h1>This is sparta</h1><p>Some text</p>;
+
+const appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
